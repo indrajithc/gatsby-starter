def6e9834f4d2ee220532c4c525fd8d8b70ede56
Indrajith C <indranspeaks@gmail.com>
2020-12-05 15:08:59 +0530

    google fonts added; https://www.gatsbyjs.com/plugins/gatsby-plugin-google-fonts/?=google%20fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,5 +38,13 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: "gatsby-plugin-google-fonts",
+      options: {
+        fonts: [
+          "roboto mono", "source sans pro\:300,400,400i,700"], // you can also specify font weights and styles
+        display: "swap",
+      },
+    },
   ],
 };

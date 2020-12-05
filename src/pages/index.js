a803343be37a75f3c3f16c0dea59af6c2520475e
Indrajith C <indranspeaks@gmail.com>
2020-12-05 15:43:58 +0530

    eslint error fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import * as React from "react";
 
 // markup
-const IndexPage = () => {
-  return (
-    <main>
-      <center>
-        <h1>Gatsby</h1>
-      </center>
-    </main>
-  );
-};
+const IndexPage = () => (
+  <main>
+    <center>
+      <h1>Gatsby</h1>
+    </center>
+  </main>
+);
 
 export default IndexPage;

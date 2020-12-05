49d3e664ee9da3f77d38cdbd10370b0253e8ecdb
Indrajith C <indranspeaks@gmail.com>
2020-12-05 15:03:57 +0530

    gatsby-remark-images for optimized images and configs; ref: https://www.youtube.com/watch?v=ylBtkaD54YM&list=PLW0RabRDhwwzVNhlOgQQgw6HJzXdM1MnT&index=5, https://www.gatsbyjs.com/plugins/gatsby-plugin-mdx/?=remark#gatsby-remark-plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,19 @@ module.exports = {
     },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        extensions: [".mdx", ".md"],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1360,
+            },
+          },
+        ],
+      },
+    },
   ],
 };

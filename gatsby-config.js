353b91b8100b012bac8948f9c6c94fbb28155ec4
Indrajith C <indranspeaks@gmail.com>
2020-12-05 14:51:48 +0530

    added gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,26 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-eslint'
-  ]
-}
+    "gatsby-plugin-eslint",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
+};

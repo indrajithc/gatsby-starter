14a004f42e45004ceee1b34619ffb5ea678a1148
Indrajith C <indranspeaks@gmail.com>
2020-12-05 16:18:05 +0530

    gatsby-plugin-styled-components added for theming

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,10 +41,10 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-fonts",
       options: {
-        fonts: [
-          "roboto mono", "source sans pro\:300,400,400i,700"], // you can also specify font weights and styles
+        fonts: ["roboto mono", "source sans pro\:300,400,400i,700"], // you can also specify font weights and styles
         display: "swap",
       },
     },
+    "gatsby-plugin-styled-components",
   ],
 };

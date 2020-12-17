691c80f675dc219dd16d45bbb1d4664f64e73efc
Indrajith C <indranspeaks@gmail.com>
2020-12-17 06:21:04 +0530

    error fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,33 +38,33 @@ exports.createPages = async function ({ actions, graphql }) {
   const numPages = Math.ceil(data.allMdx.edges.length / postPerPage);
 
   Array.from({
-    length: numPages
-  }).forEach(_, index) => {
+    length: numPages,
+  }).forEach((_, index) => {
     actions.createPages({
-      path: index === 0 ? `/` : `/${i + 1}`,
+      path: index === 0 ? "/" : `/${i + 1}`,
       component: require.resolve("./src/templates/allPosts.js"),
       context: {
         limit: postPerPage,
         skip: index * postPerPage,
         numPages,
-        currentPage: index + 1
-      }
-    })
-  }
+        currentPage: index + 1,
+      },
+    });
+  });
 
   /**
    * Single post page
    */
 
   data.allMdx.edges.forEach((edge) => {
-    const slug = edge.node.frontmatter.slug;
-    const id = edge.node.frontmatter.id;
+    const { slug } = edge.node.frontmatter;
+    const { id } = edge.node.frontmatter;
     actions.createPages({
       path: slug,
       component: require.resolve("./src/templates/singlePost.js"),
       context: {
-        id
-      }
-    })
+        id,
+      },
+    });
   });
 };
